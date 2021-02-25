de8ab618935ef960dc74263240dff4dd8e58de1e
sangle173 <leducsang.10dt2@gmail.com>
2021-02-25 08:33:41 +0700

    update lesson clean code

---
+++ src/_08_clean_code_refactor/practises/Cylinder.java
@@ -0,0 +1,18 @@
+package _08_clean_code_refactor.practises;
+
+public class Cylinder {
+    public static double getVolume(int radius, int height){
+        double baseArea = getBaseArea(radius);
+        double perimeter = getPerimeter(radius);
+        double volume = perimeter * height + 2 * baseArea;
+        return volume;
+    }
+
+    public static double getPerimeter(double radius) {
+        return 2 * Math.PI  * radius;
+    }
+
+    public static double getBaseArea(double radius) {
+        return Math.PI * radius * radius;
+    }
+}
